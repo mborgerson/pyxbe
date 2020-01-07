51d1b40bd32e125764e7bda880b7eb26bb495d69
Matt Borgerson <contact@mborgerson.com>
2020-01-06 17:25:37 -0700

    Use README.md contents as setup.py's long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
+with open('README.md') as f:
+	long_description = f.read()
+
 setup(name='pyxbe',
       version='0.0.1',
       description='Library to work with XBE files',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Matt Borgerson',
       author_email='contact@mborgerson.com',
       url='https://github.com/mborgerson/pyxbe',
