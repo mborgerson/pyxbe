c3d776f6bf55892705ed7bc61349954c7d9ebb1e
Matt Borgerson <contact@mborgerson.com>
2023-03-14 21:13:04 -0700

    Migrate setup() fields to setup.cfg

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-with open("README.md") as f:
-    long_description = f.read()
-
-setup(
-    name="pyxbe",
-    version="1.0.1",
-    description="Library to work with XBE files",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Matt Borgerson",
-    author_email="contact@mborgerson.com",
-    url="https://github.com/mborgerson/pyxbe",
-    license="MIT",
-    packages=["xbe"],
-    package_data={"xbe": ["py.typed"]},
-)
+setup()
