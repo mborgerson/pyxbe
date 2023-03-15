04637daba17a67735e16e75b31d30064538b46cb
Matt Borgerson <contact@mborgerson.com>
2023-03-14 20:25:28 -0700

    Run black formatter

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-with open('README.md') as f:
-	long_description = f.read()
+with open("README.md") as f:
+    long_description = f.read()
 
-setup(name='pyxbe',
-      version='1.0.1',
-      description='Library to work with XBE files',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Matt Borgerson',
-      author_email='contact@mborgerson.com',
-      url='https://github.com/mborgerson/pyxbe',
-      license='MIT',
-      packages=['xbe'],
-     )
+setup(
+    name="pyxbe",
+    version="1.0.1",
+    description="Library to work with XBE files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Matt Borgerson",
+    author_email="contact@mborgerson.com",
+    url="https://github.com/mborgerson/pyxbe",
+    license="MIT",
+    packages=["xbe"],
+)
